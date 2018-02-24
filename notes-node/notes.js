2555fc7432dface45a3b2fb02c084316577e3acc
Aditya Deorha <aditya.deorha1@gmail.com>
2018-02-24 08:45:50 -0800

    Added the add functionality which adds the title/body of the book in an external JSON file. Write and read back from the JSON file and converting it back and forth to an object. Also checking for duplicate titles and then not adding them.

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,25 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-    console.log('Adding note', title, body);
+    var notes = [];
+    var note = {
+      title,
+      body
+    };
+
+    try{
+      notes = JSON.parse(fs.readFileSync('notes-data.json'));
+    } catch(e){
+
+    }
+    var duplicateNotes = notes.filter((note) => note.title === title);
+    if(duplicateNotes.length === 0)
+    {
+      notes.push(note);
+      fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
 };
 
 var getAll = () => {
