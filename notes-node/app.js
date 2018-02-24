6b256767565027cea22fe21d1180cf4f96655808
Aditya Deorha <aditya.deorha1@gmail.com>
2018-02-24 12:39:58 -0800

    Did the code refactoring.

--- notes-node/app.js
+++ notes-node/app.js
@@ -13,7 +13,16 @@ console.log('Command: ', command);
 console.log('Yargs: ', argv);
 
 if(command === 'add'){
-  notes.addNote(argv.title, argv.body);
+  var note = notes.addNote(argv.title, argv.body);
+  if(note){
+    console.log('Node added successfully');
+    console.log('--');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+  }
+  else {
+    console.log('Note title already taken');
+  }
 }
 else if(command === 'list'){
   notes.getAll();
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -2,23 +2,31 @@ console.log('Starting notes.js');
 
 const fs = require('fs');
 
+var fetchNotes = () => {
+  try{
+    return JSON.parse(fs.readFileSync('notes-data.json'));
+  } catch(e){
+    return [];
+  }
+}
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+}
+
 var addNote = (title, body) => {
-    var notes = [];
+    var notes = fetchNotes();
     var note = {
       title,
       body
     };
 
-    try{
-      notes = JSON.parse(fs.readFileSync('notes-data.json'));
-    } catch(e){
-
-    }
     var duplicateNotes = notes.filter((note) => note.title === title);
     if(duplicateNotes.length === 0)
     {
       notes.push(note);
-      fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+      saveNotes(notes);
+      return note;
     }
 };
 
@@ -31,7 +39,11 @@ var getNote = (title) => {
 };
 
 var removeNote = (title) => {
-  console.log('Removing note', title);
+  var notes = fetchNotes();
+
+  var newNotes = notes.filter((note) => note.title !== title);
+  var notes = newNotes;
+  saveNotes(notes);
 };
 
 module.exports = {
