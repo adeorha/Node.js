a054258ef9689d0101e6fca131a8af1bae9c0317
Aditya Deorha <aditya.deorha1@gmail.com>
2018-02-23 22:17:34 -0800

    Cleaned up comments and used process.argv to see the user arguments. Will next use yargs.

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,16 +1,25 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-var filteredArray = _.uniq(['Aditya', 1, 'Aditya', 1, 2, 3, 4]);
+var command = process.argv[2];
+console.log('Command: ', command);
 
-console.log(filteredArray);
-//var res = notes.addNote();
-// console.log('Result: ',notes.add(3,4));
-// var user = os.userInfo();
-
-
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
+if(command === 'add'){
+  console.log('Adding new note');
+}
+else if(command === 'list'){
+  console.log('Listing all notes');
+}
+else if(command === 'read'){
+  console.log('Reading note');
+}
+else if(command === 'remove'){
+  console.log('Removing note');
+}
+else{
+  console.log('Command not recognized');
+}
