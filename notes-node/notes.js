f1b769b717dee490af05a2d4ae015bb1f69200d8
Aditya Deorha <aditya.deorha1@gmail.com>
2018-02-23 20:02:11 -0800

    Added a couple of functions using arguments and => operator instead of writing 'function'

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,3 +1,10 @@
 console.log('Starting notes.js');
 
-module.exports.age = 30;
+module.exports.addNote = () => {
+    console.log('addNote');
+    return 'New note';
+};
+
+module.exports.add = (a,b) => {
+  return (a+b);
+}
