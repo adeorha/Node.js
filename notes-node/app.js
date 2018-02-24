67f947ecd6af1c2cc8e5a46a9896e6cb33ad923a
Aditya Deorha <aditya.deorha1@gmail.com>
2018-02-23 17:18:23 -0800

    Requiring notes.js and using the age property exported from there.

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,9 +1,10 @@
-console.log('Starting app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
 var user = os.userInfo();
 
 
-fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
+fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
