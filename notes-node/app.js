d941e37852a4f2263c4169ee35bb746369cd60d7
Aditya Deorha <aditya.deorha1@gmail.com>
2018-02-24 07:45:08 -0800

    Used yargs to get user input and added add, list, listAll and remove functionalities in notes.

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,23 +2,27 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-var command = process.argv[2];
+const argv = yargs.argv;
+
+var command = argv._[0];
 console.log('Command: ', command);
+console.log('Yargs: ', argv);
 
 if(command === 'add'){
-  console.log('Adding new note');
+  notes.addNote(argv.title, argv.body);
 }
 else if(command === 'list'){
-  console.log('Listing all notes');
+  notes.getAll();
 }
 else if(command === 'read'){
-  console.log('Reading note');
+  notes.getNote(argv.title);
 }
 else if(command === 'remove'){
-  console.log('Removing note');
+  notes.removeNote(argv.title);
 }
 else{
   console.log('Command not recognized');
