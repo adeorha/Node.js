2ba9e28e98184978f911355000400290ac79035e
Aditya Deorha <aditya.deorha1@gmail.com>
2018-02-23 20:02:46 -0800

    Added lodash and used the .uniq feature of the same.

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,9 +2,15 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var user = os.userInfo();
+var filteredArray = _.uniq(['Aditya', 1, 'Aditya', 1, 2, 3, 4]);
+
+console.log(filteredArray);
+//var res = notes.addNote();
+// console.log('Result: ',notes.add(3,4));
+// var user = os.userInfo();
 
 
-fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
+// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
