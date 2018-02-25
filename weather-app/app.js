a24e9f303fd14b43477dfbef36988f0ba3888fbe
Aditya Deorha <aditya.deorha1@gmail.com>
2018-02-25 14:37:08 -0800

    Got the address from user input and then used encodeURIComponent

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,15 +1,29 @@
 const request = require('request');
+const yargs = require('yargs');
 
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      desc: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
 
+  var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: 'http://maps.googleapis.com/maps/api/geocode/json?address=395%20Ano%20Nuevo%20Ave',
+  url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
 //  console.log(error);
 //  console.log(response.statusCode);
-console.log('Formatted address = '+JSON.stringify(body.results[0].formatted_address, undefined, 2));
-console.log('Latitude = '+JSON.stringify(body.results[0].geometry.location.lat, undefined, 2));
-console.log('Longitude = '+JSON.stringify(body.results[0].geometry.location.lng, undefined, 2));
+console.log(`Address = ${body.results[0].formatted_address}`);
+console.log(`Latitude = ${body.results[0].geometry.location.lat}`);
+console.log(`Longitude = ${body.results[0].geometry.location.lng}`);
 
 })
