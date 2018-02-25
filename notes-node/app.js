bdca5cc5e0c0ab8d01dff5aa05b80eeb9e1b6469
Aditya Deorha <aditya.deorha1@gmail.com>
2018-02-25 10:29:25 -0800

    Used advanced yargs argument to describe what a valid command line argument can be.

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,37 +1,67 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes.js');
+const titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+}
+const bodyOptions = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+}
 
-const argv = yargs.argv;
+const argv = yargs
+.command('add', 'Adding a note', {
+  title: titleOptions,
+  body: bodyOptions
+})
+.command('list', 'List all notes')
+.command('read', 'Read a note', {
+  title: titleOptions
+})
+.command('remove', 'Removing a note', {
+  title: titleOptions
+})
+.help()
+.argv;
+
+
 
 var command = argv._[0];
-console.log('Command: ', command);
-console.log('Yargs: ', argv);
 
 if(command === 'add'){
   var note = notes.addNote(argv.title, argv.body);
   if(note){
     console.log('Node added successfully');
-    console.log('--');
-    console.log(`Title: ${note.title}`);
-    console.log(`Body: ${note.body}`);
+    notes.logNote(note);
   }
   else {
     console.log('Note title already taken');
   }
 }
 else if(command === 'list'){
-  notes.getAll();
+  var allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} notes`);
+    allNotes.forEach((note) => notes.logNote(note));
 }
 else if(command === 'read'){
-  notes.getNote(argv.title);
+  var note = notes.getNote(argv.title);
+  if(note){
+    console.log('Node found');
+    notes.logNote(note);
+  }
+  else {
+    console.log('Node not found');
+  }
 }
 else if(command === 'remove'){
-  notes.removeNote(argv.title);
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? 'Note was removed' : 'Note not found';
+  console.log(message);
 }
 else{
   console.log('Command not recognized');
