348db269eede519c55170fcdb45f3cb5d075dfcc
Aditya Deorha <aditya.deorha1@gmail.com>
2018-02-28 17:18:40 -0800

    Changed appendFile to appendFileSync in order to remove the warning for calling asynchronous function without callback.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   var log = `${now}: ${req.method} ${req.url}`;
 
   console.log(log);
-  fs.appendFile('server.log', log + '\n');
+  fs.appendFileSync('server.log', log + '\n');
   next();
 });
 
